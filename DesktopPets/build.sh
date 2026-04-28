8ec41aaf06790ebfe0b2b0fb6a958c10ca22fab2
Fei-012 <jzdssyysv2@privaterelay.appleid.com>
2026-04-28 15:08:24 +0100

    mm

--- DesktopPets/build.sh
+++ DesktopPets/build.sh
@@ -12,13 +12,30 @@ RESOURCES_DIR="$CONTENTS_DIR/Resources"
 MODULE_CACHE_DIR="$BUILD_DIR/swift-module-cache"
 ARM64_BIN="$BUILD_DIR/DesktopPets-arm64"
 X64_BIN="$BUILD_DIR/DesktopPets-x86_64"
+ICONSET_DIR="$BUILD_DIR/AppIcon.iconset"
+ICON_PNG="$ROOT_DIR/Icon.png"
+ICON_ICNS="$RESOURCES_DIR/AppIcon.icns"
 
 rm -rf "$APP_DIR"
+rm -rf "$ICONSET_DIR"
 mkdir -p "$MACOS_DIR" "$RESOURCES_DIR" "$MODULE_CACHE_DIR"
 
 cp "$PROJECT_DIR/Info.plist" "$CONTENTS_DIR/Info.plist"
 cp "$ROOT_DIR/IMG_3090.GIF" "$RESOURCES_DIR/IMG_3090.GIF"
 cp "$ROOT_DIR/UntitledArtwork1.gif" "$RESOURCES_DIR/UntitledArtwork1.gif"
+
+mkdir -p "$ICONSET_DIR"
+sips -z 16 16     "$ICON_PNG" --out "$ICONSET_DIR/icon_16x16.png" >/dev/null
+sips -z 32 32     "$ICON_PNG" --out "$ICONSET_DIR/icon_16x16@2x.png" >/dev/null
+sips -z 32 32     "$ICON_PNG" --out "$ICONSET_DIR/icon_32x32.png" >/dev/null
+sips -z 64 64     "$ICON_PNG" --out "$ICONSET_DIR/icon_32x32@2x.png" >/dev/null
+sips -z 128 128   "$ICON_PNG" --out "$ICONSET_DIR/icon_128x128.png" >/dev/null
+sips -z 256 256   "$ICON_PNG" --out "$ICONSET_DIR/icon_128x128@2x.png" >/dev/null
+sips -z 256 256   "$ICON_PNG" --out "$ICONSET_DIR/icon_256x256.png" >/dev/null
+sips -z 512 512   "$ICON_PNG" --out "$ICONSET_DIR/icon_256x256@2x.png" >/dev/null
+sips -z 512 512   "$ICON_PNG" --out "$ICONSET_DIR/icon_512x512.png" >/dev/null
+sips -z 1024 1024 "$ICON_PNG" --out "$ICONSET_DIR/icon_512x512@2x.png" >/dev/null
+iconutil -c icns "$ICONSET_DIR" -o "$ICON_ICNS"
 
 swiftc \
   -module-cache-path "$MODULE_CACHE_DIR" \
